f3f0ff497bf9e340adc90854eac3b71cee908d38
Berty Li <46299378+li-berty@users.noreply.github.com>
2020-05-27 00:57:46 +0300

    Default.ps1

---
+++ Default.ps1
@@ -0,0 +1,42 @@
+###############################################################################################################
+# Language     :  PowerShel
+# Filename     :  
+# Autor        :  Berty Li (https://github.com/li-berty)
+# Description  :  
+# Version      : 
+# Repository   :  https://github.com/li-berty/--ENTERNAMEHERE--
+###############################################################################################################
+#
+# Changelog
+#
+###############################################################################################################
+
+<#
+    .SYNOPSIS
+    
+    .DESCRIPTION
+    
+    .EXAMPLE
+        
+    .EXAMPLE
+    
+    .LINK
+    https://github.com/li-berty/--ENTERNAMEHERE--
+#>
+
+[CmdletBinding()]
+param(
+
+)
+
+Begin{
+
+}
+
+Process{
+
+}
+
+End{
+
+}
